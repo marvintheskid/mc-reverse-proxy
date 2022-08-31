136c017b0fc580fec4bc5df3967d0f7eff6836c7
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2022-08-31 17:24:07 +0200

    removed unnecessary methods from MinecraftEncryption

--- src/main/java/gbx/proxy/utils/MinecraftEncryption.java
+++ src/main/java/gbx/proxy/utils/MinecraftEncryption.java
@@ -5,39 +5,30 @@ import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Cipher-related utils.
  */
 public interface MinecraftEncryption {
     /**
-     * Generates the required keypair for packet encryption.
+     * Generates the required secret key for packet encryption.
      *
      * @return the keypair
      */
-    static KeyPair generateKeyPairs() {
+    static SecretKey generateSecretKey() {
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(1024);
-            return keyPairGenerator.generateKeyPair();
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128);
+            return keyGenerator.generateKey();
         } catch (NoSuchAlgorithmException ex) {
-            ex.printStackTrace();
-            return null;
+            throw new RuntimeException(ex);
         }
     }
 
@@ -53,8 +44,62 @@ public interface MinecraftEncryption {
         try {
             return operateDigest("SHA-1", serverId.getBytes("ISO_8859_1"), secretKey.getEncoded(), publicKey.getEncoded());
         } catch (UnsupportedEncodingException ex) {
-            ex.printStackTrace();
-            return null;
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Creates (reconstitutes) a public RSA key using the given encoded key.
+     *
+     * @param encodedKey the encoded key
+     * @return a new RSA public key
+     */
+    static PublicKey createRSAPublicKey(byte[] encodedKey) {
+        try {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encodedKey);
+            KeyFactory rsaFactory = KeyFactory.getInstance("RSA");
+            return rsaFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Decrypts the given data using the given key.
+     *
+     * @param key the key
+     * @param data the data
+     * @return the decrypted data
+     */
+    static byte[] decryptData(Key key, byte[] data) { // Feather: renamed method: b(Key, byte[]) -> decryptData(Key, byte[])
+        return createAndOperate(Cipher.DECRYPT_MODE, key, data);
+    }
+
+    /**
+     * Encrypts the given data using the given key.
+     *
+     * @param key the key
+     * @param data the data
+     * @return the encrypted data
+     */
+    static byte[] encryptData(Key key, byte[] data) { // Feather: renamed method: b(Key, byte[]) -> decryptData(Key, byte[])
+        return createAndOperate(Cipher.ENCRYPT_MODE, key, data);
+    }
+
+    /**
+     * Creates an AES/CFB8/NoPadding cipher used for packet encryption / decryption.
+     *
+     * @param mode the mode
+     * @param key the key
+     * @return the cipher
+     */
+    static Cipher createEncryptionCipher(int mode, Key key) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding");
+            cipher.init(mode, key, new IvParameterSpec(key.getEncoded()));
+            return cipher;
+        } catch (GeneralSecurityException ex) {
+            throw new RuntimeException(ex);
         }
     }
 
@@ -75,47 +120,8 @@ public interface MinecraftEncryption {
 
             return digest.digest();
         } catch (NoSuchAlgorithmException ex) {
-            ex.printStackTrace();
-            return null;
+            throw new RuntimeException(ex);
         }
-    }
-
-    /**
-     * Creates (reconstitutes) a public RSA key using the given encoded key.
-     *
-     * @param encodedKey the encoded key
-     * @return a new RSA public key
-     */
-    static PublicKey createRSAPublicKey(byte[] encodedKey) {
-        try {
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encodedKey);
-            KeyFactory rsaFactory = KeyFactory.getInstance("RSA");
-            return rsaFactory.generatePublic(keySpec);
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException ignored) {
-            return null;
-        }
-    }
-
-    /**
-     * Decrypts the given shared secret AES key using an RSA private key.
-     *
-     * @param privateKey the private RSA key
-     * @param encryptedKey the encrypted key
-     * @return the shared secret
-     */
-    static SecretKey decryptSharedKey(PrivateKey privateKey, byte[] encryptedKey) {
-        return new SecretKeySpec(decryptData(privateKey, encryptedKey), "AES");
-    }
-
-    /**
-     * Decrypts the given data using the given key.
-     *
-     * @param key the key
-     * @param data the data
-     * @return the decrypted data
-     */
-    static byte[] decryptData(Key key, byte[] data) { // Feather: renamed method: b(Key, byte[]) -> decryptData(Key, byte[])
-        return createAndOperate(Cipher.DECRYPT_MODE, key, data);
     }
 
     /**
@@ -130,8 +136,7 @@ public interface MinecraftEncryption {
         try {
             return createCipher(key.getAlgorithm(), mode, key).doFinal(data);
         } catch (IllegalBlockSizeException | BadPaddingException ex) {
-            ex.printStackTrace();
-            return null;
+            throw new RuntimeException(ex);
         }
     }
 
@@ -149,24 +154,6 @@ public interface MinecraftEncryption {
             cipher.init(mode, key);
             return cipher;
         } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * Creates an AES/CFB8/NoPadding cipher used for packet encryption / decryption.
-     *
-     * @param mode the mode
-     * @param key the key
-     * @return the cipher
-     */
-    static Cipher createEncryptionCipher(int mode, Key key) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding");
-            cipher.init(mode, key, new IvParameterSpec(key.getEncoded()));
-            return cipher;
-        } catch (GeneralSecurityException ex) {
             throw new RuntimeException(ex);
         }
     }
