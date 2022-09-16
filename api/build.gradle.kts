d13ce767a83616f3f04dfe7bafce9f6b65585be8
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2022-09-17 00:07:33 +0200

    added log4j as a dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,9 +3,12 @@ dependencies {
     val annotationsVersion = "23.0.0"
     val gsonVersion = "2.9.1"
     val authlibVersion = "3.5.41"
+    val log4jVersion = "2.18.0"
 
     api(group = "io.netty", name = "netty-all", version = nettyVersion)
     api(group = "org.jetbrains", name = "annotations", version = annotationsVersion)
     api(group = "com.google.code.gson", name = "gson", version = gsonVersion)
     api(group = "com.mojang", name = "authlib", version = authlibVersion)
+    api(group = "org.apache.logging.log4j", name = "log4j-api", version = log4jVersion)
+    api(group = "org.apache.logging.log4j", name = "log4j-core", version = log4jVersion)
 }
