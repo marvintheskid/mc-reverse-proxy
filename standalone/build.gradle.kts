9d9834a673cf1d85dde216f73a0bf509f48b271d
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2022-09-17 00:19:09 +0200

    added jline and tca as a dependency

--- standalone/build.gradle.kts
+++ standalone/build.gradle.kts
@@ -1,7 +1,15 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 dependencies {
+    val jlineVersion = "3.21.0"
+    val tcaVersion = "1.3.0"
+
     implementation(project(":api"))
+
+    api(group = "org.jline", name = "jline-reader", version = jlineVersion)
+    api(group = "org.jline", name = "jline-terminal", version = jlineVersion)
+    api(group = "org.jline", name = "jline-terminal-jansi", version = jlineVersion)
+    api(group = "net.minecrell", name = "terminalconsoleappender", version = tcaVersion)
 }
 
 tasks.withType<ShadowJar> {
