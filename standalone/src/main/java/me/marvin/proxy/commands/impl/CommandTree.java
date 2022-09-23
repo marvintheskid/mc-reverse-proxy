ebb620cd4b2efbee59b8069bf0208f3d634a754a
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2022-09-23 19:35:20 +0200

    very cursed CommandTree#unregister

--- standalone/src/main/java/me/marvin/proxy/commands/impl/CommandTree.java
+++ standalone/src/main/java/me/marvin/proxy/commands/impl/CommandTree.java
@@ -4,6 +4,8 @@ import me.marvin.proxy.commands.Command;
 import me.marvin.proxy.utils.Tristate;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiConsumer;
 
 public class CommandTree extends CommandEntry {
     public void register(Command command, String... literals) {
@@ -25,14 +27,36 @@ public class CommandTree extends CommandEntry {
         }
     }
 
+    public void unregister(String... literals) {
+        for (String literal : literals) {
+            AtomicBoolean first = new AtomicBoolean(true);
+            traverseTree(literal, (key, entry) -> {
+                if (entry instanceof ExecutableCommandEntry parent) {
+                    ExecutableCommandEntry realEntry = parent.children.get(key);
+
+                    if (realEntry != null) {
+                        if (first.get()) {
+                            realEntry.command(null);
+                            first.set(false);
+                        }
+
+                        if (realEntry.children.isEmpty()) {
+                            parent.children.remove(key);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
     public Tristate execute(String literal) {
         String[] parts = literal.split(" ");
-        ExecutableCommandEntry current = null;
-        int start = parts.length;
+        CommandEntry current = this;
+        int start = -1;
 
         for (int i = 0; i < parts.length; i++) {
             String part = parts[i];
-            ExecutableCommandEntry next = children.get(part);
+            ExecutableCommandEntry next = current.children.get(part);
 
             if (next == null) {
                 start = i;
@@ -42,11 +66,41 @@ public class CommandTree extends CommandEntry {
             }
         }
 
-        if (current != null) {
-            parts = Arrays.copyOfRange(parts, start, parts.length);
-            return Tristate.fromBoolean(current.command().execute(parts));
+        if (current instanceof ExecutableCommandEntry executable) {
+            parts = Arrays.copyOfRange(parts, start == -1 ? parts.length : start, parts.length);
+            return Tristate.fromBoolean(executable.command().execute(parts));
         }
 
         return Tristate.NOT_SET;
+    }
+
+    private void traverseTree(String literal, BiConsumer<String, CommandEntry> consumer) {
+        String[] parts = literal.split(" ");
+        CommandEntry current = this;
+        int start = -1;
+
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+            ExecutableCommandEntry next = current.children.get(part);
+
+            if (next == null) {
+                start = i - 1;
+                break;
+            } else if (i == parts.length - 1) {
+                start = i;
+                break;
+            } else {
+                current = next;
+            }
+        }
+
+        if (current instanceof ExecutableCommandEntry executable) {
+            CommandEntry parent = current;
+            for (int i = start; i >= 0; i--) {
+                String part = parts[i];
+                consumer.accept(part, parent);
+                parent = executable.parent();
+            }
+        }
     }
 }
